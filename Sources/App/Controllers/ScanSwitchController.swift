72a3aedfb463a6fd3d4249923b58fcfe0cdefb66
alexclp <alexandru.clapa@gmail.com>
2018-01-25 14:32:57 +0000

    Added updateable flag

--- Sources/App/Controllers/ScanSwitchController.swift
+++ Sources/App/Controllers/ScanSwitchController.swift
@@ -12,28 +12,20 @@ final class ScanSwitchController: ResourceRepresentable {
         return scanSwitch
     }
 
-    /// When the consumer calls 'GET' on a specific resource, ie:
-    /// '/posts/13rd88' we should show that specific post
     func show(_ req: Request, scanSwitch: ScanSwitch) throws -> ResponseRepresentable {
         return scanSwitch
     }
 
-    /// When the consumer calls 'DELETE' on a specific resource, ie:
-    /// 'posts/l2jd9' we should remove that resource from the database
     func delete(_ req: Request, scanSwitch: ScanSwitch) throws -> ResponseRepresentable {
         try scanSwitch.delete()
         return Response(status: .ok)
     }
 
-    /// When the consumer calls 'DELETE' on the entire table, ie:
-    /// '/posts' we should remove the entire table
     func clear(_ req: Request) throws -> ResponseRepresentable {
         try ScanSwitch.makeQuery().delete()
         return Response(status: .ok)
     }
 
-    /// When the user calls 'PATCH' on a specific resource, we should
-    /// update that resource to the new values.
     func update(_ req: Request, scanSwitch: ScanSwitch) throws -> ResponseRepresentable {
         // See `extension Post: Updateable`
         try scanSwitch.update(for: req)
@@ -43,27 +35,15 @@ final class ScanSwitchController: ResourceRepresentable {
         return scanSwitch
     }
 
-    /// When a user calls 'PUT' on a specific resource, we should replace any
-    /// values that do not exist in the request with null.
-    /// This is equivalent to creating a new Post with the same ID.
     func replace(_ req: Request, scanSwitch: ScanSwitch) throws -> ResponseRepresentable {
-        // First attempt to create a new Post from the supplied JSON.
-        // If any required fields are missing, this request will be denied.
         let new = try req.createEntity()
 
-        // Update the post with all of the properties from
-        // the new post
         scanSwitch.shouldScan = new.shouldScan
         try scanSwitch.save()
 
-        // Return the updated post
         return scanSwitch
     }
 
-    /// When making a controller, it is pretty flexible in that it
-    /// only expects closures, this is useful for advanced scenarios, but
-    /// most of the time, it should look almost identical to this
-    /// implementation
     func makeResource() -> Resource<ScanSwitch> {
         return Resource(
             index: index,
@@ -78,17 +58,10 @@ final class ScanSwitchController: ResourceRepresentable {
 }
 
 extension Request {
-    /// Create a post from the JSON body
-    /// return BadRequest error if invalid
-    /// or no JSON
     func createEntity() throws -> ScanSwitch {
         guard let json = json else { throw Abort.badRequest }
         return try ScanSwitch(json: json)
     }
 }
 
-/// Since PostController doesn't require anything to
-/// be initialized we can conform it to EmptyInitializable.
-///
-/// This will allow it to be passed by type.
 extension ScanSwitchController: EmptyInitializable { }
--- Sources/App/Models/ScanSwitch.swift
+++ Sources/App/Models/ScanSwitch.swift
@@ -7,29 +7,23 @@ final class ScanSwitch: Model {
 
     // MARK: Properties and database keys
 
-    /// The content of the post
     var shouldScan: Bool
 
-    /// The column names for `id` and `content` in the database
     struct Keys {
         static let id = "id"
         static let shouldScan = "shouldScan"
     }
 
-    /// Creates a new Post
     init(shouldScan: Bool) {
         self.shouldScan = shouldScan
     }
 
     // MARK: Fluent Serialization
 
-    /// Initializes the Post from the
-    /// database row
     init(row: Row) throws {
         shouldScan = try row.get(ScanSwitch.Keys.shouldScan)
     }
 
-    // Serializes the Post to the database
     func makeRow() throws -> Row {
         var row = Row()
         try row.set(ScanSwitch.Keys.shouldScan, shouldScan)
@@ -41,7 +35,6 @@ final class ScanSwitch: Model {
 
 extension ScanSwitch: Preparation {
     /// Prepares a table/collection in the database
-    /// for storing Posts
     static func prepare(_ database: Database) throws {
         try database.create(self) { builder in
             builder.id()
@@ -55,13 +48,6 @@ extension ScanSwitch: Preparation {
     }
 }
 
-// MARK: JSON
-
-// How the model converts from / to JSON.
-// For example when:
-//     - Creating a new Post (POST /posts)
-//     - Fetching a post (GET /posts, GET /posts/:id)
-//
 extension ScanSwitch: JSONConvertible {
     func makeJSON() throws -> JSON {
         var json = JSON()
@@ -81,21 +67,13 @@ extension ScanSwitch: JSONInitializable {
 
 // MARK: HTTP
 
-// This allows Post models to be returned
-// directly in route closures
 extension ScanSwitch: ResponseRepresentable { }
 
 // MARK: Update
 
-// This allows the Post model to be updated
-// dynamically by the request.
 extension ScanSwitch: Updateable {
-    // Updateable keys are called when `post.update(for: req)` is called.
-    // Add as many updateable keys as you like here.
     public static var updateableKeys: [UpdateableKey<ScanSwitch>] {
         return [
-            // If the request contains a String at key "content"
-            // the setter callback will be called.
             UpdateableKey(ScanSwitch.Keys.shouldScan, Bool.self) { scanSwitch, shouldScan in
                 scanSwitch.shouldScan = shouldScan
             }

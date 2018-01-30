edc716c44a15bb5f2eb1473b961dfdfee0b327db
alexclp <alexandru.clapa@gmail.com>
2018-01-30 12:15:00 +0000

    Added room ID and location ID

--- Sources/App/Models/ScanSwitch.swift
+++ Sources/App/Models/ScanSwitch.swift
@@ -8,25 +8,35 @@ final class ScanSwitch: Model {
     // MARK: Properties and database keys
 
     var shouldScan: Bool
+    var roomID: Int
+    var locationID: Int
 
     struct Keys {
         static let id = "id"
         static let shouldScan = "shouldScan"
+        static let roomID = "roomID"
+        static let locationID = "locationID"
     }
 
-    init(shouldScan: Bool) {
+    init(shouldScan: Bool, roomID: Int, locationID: Int) {
         self.shouldScan = shouldScan
+        self.roomID = roomID
+        self.locationID = locationID
     }
 
     // MARK: Fluent Serialization
 
     init(row: Row) throws {
         shouldScan = try row.get(ScanSwitch.Keys.shouldScan)
+        roomID = try row.get(ScanSwitch.Keys.roomID)
+        locationID = try row.get(ScanSwitch.Keys.locationID)
     }
 
     func makeRow() throws -> Row {
         var row = Row()
         try row.set(ScanSwitch.Keys.shouldScan, shouldScan)
+        try row.set(ScanSwitch.Keys.roomID, roomID)
+        try row.set(ScanSwitch.Keys.locationID, locationID)
         return row
     }
 }
@@ -39,6 +49,8 @@ extension ScanSwitch: Preparation {
         try database.create(self) { builder in
             builder.id()
             builder.bool(ScanSwitch.Keys.shouldScan)
+            builder.int(ScanSwitch.Keys.roomID)
+            builder.int(ScanSwitch.Keys.locationID)
         }
     }
 
@@ -53,6 +65,8 @@ extension ScanSwitch: JSONConvertible {
         var json = JSON()
         try json.set(ScanSwitch.Keys.id, id)
         try json.set(ScanSwitch.Keys.shouldScan, shouldScan)
+        try json.set(ScanSwitch.Keys.roomID, roomID)
+        try json.set(ScanSwitch.Keys.locationID, locationID)
         return json
     }
 }
@@ -60,7 +74,9 @@ extension ScanSwitch: JSONConvertible {
 extension ScanSwitch: JSONInitializable {
     convenience init(json: JSON) throws {
         self.init(
-            shouldScan: try json.get(ScanSwitch.Keys.shouldScan)
+            shouldScan: try json.get(ScanSwitch.Keys.shouldScan),
+            roomID: try json.get(ScanSwitch.Keys.roomID),
+            locationID: try json.get(ScanSwitch.Keys.locationID)
         )
     }
 }
@@ -76,6 +92,12 @@ extension ScanSwitch: Updateable {
         return [
             UpdateableKey(ScanSwitch.Keys.shouldScan, Bool.self) { scanSwitch, shouldScan in
                 scanSwitch.shouldScan = shouldScan
+            },
+            UpdateableKey(ScanSwitch.Keys.roomID, Int.self) { scanSwitch, roomID in
+                scanSwitch.roomID = roomID
+            },
+            UpdateableKey(ScanSwitch.Keys.locationID, Int.self) { scanSwitch, locationID in
+                scanSwitch.locationID = locationID
             }
         ]
     }
